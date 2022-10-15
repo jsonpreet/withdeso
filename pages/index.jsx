aa5b6cb7f9bc2ebb652099801877ba787e5d31a9
MrPreet <harpreet9629@gmail.com>
2022-10-15 21:26:06 +0530

    helloooo

--- pages/index.jsx
+++ pages/index.jsx
@@ -39,6 +39,9 @@ const Home = () => {
             <h3 className='font-semibold ml-2 text-[#daedff]'>/</h3>
             <h3 className='font-semibold ml-2 text-[#daedff]'>Embed</h3>
           </div>
+          <div className='flex flex-row items-end justify-end'>
+            <a href='https://embed.withdeso.com' rel='noopener noreferrer nofollow' className='bg-[#ffda59] text-[#6d4800] hover:bg-[#ffcf26] font-bold py-4 px-4 rounded-tr-full rounded-br-full'>Get Embed</a>
+          </div>
         </div>          
       </div>
       <div className='flex flex-col w-full items-center'>
